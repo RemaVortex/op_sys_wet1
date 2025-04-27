e77c15e862f0e2ac92f2306b575deaa0aa93ee4f
RemaVortex <rema.drawshe@campus.technion.ac.il>
2025-04-27 18:38:12 +0300

    Update signals.h

--- smash/signals.h
+++ smash/signals.h
@@ -5,8 +5,19 @@
 * includes, defines, usings
 =============================================================================*/
 
+
+
+#include <sys/wait.h>
+#include <unistd.h>     // For kill
+#include <sys/types.h>  // For pid_t
+#include <signal.h>     // For sigaction, SA_RESTART, SIGKILL, SIGSTOP
+#include <stdio.h>
+#include "commands.h"
+
 /*=============================================================================
 * global functions
 =============================================================================*/
-
+void setupSignal_Handlers(void);
+void handle_SIGTSTOP(int signal);
+void handle_SIGINIT(int signal);
 #endif //__SIGNALS_H__
